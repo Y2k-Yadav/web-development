fcbc2e83c4ecf8829669d6a2ef9ac3265a8968d2
YadavDevCraft <63380531+Y2k-Yadav@users.noreply.github.com>
2026-07-08 16:47:20 +0530

    Add files via upload

---
+++ Tailwind css/Lect 22/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      fontSize: {
+        "10xl": "11rem"
+      },
+      fontWeight:{
+        "xl":800
+      }
+    },
+  },
+  plugins: [],
+}
