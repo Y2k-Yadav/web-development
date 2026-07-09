468b0030d9b6ca5f25a359e1f8f774669b88cc01
YadavDevCraft <63380531+Y2k-Yadav@users.noreply.github.com>
2026-07-09 16:41:03 +0530

    Add files via upload

---
+++ Tailwind css/PayTM Project/Part-1 Paytm Layout and structure/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "darkblue":"#002970",
+        "profilecolor":"#00BAF2",
+        "orginal":"#00baf2",
+        "lightcolor":"#f5f5f5",
+      },
+      width:{
+        "27.5":"6.5rem",
+      }
+    },
+  },
+  plugins: [],
+}
+
