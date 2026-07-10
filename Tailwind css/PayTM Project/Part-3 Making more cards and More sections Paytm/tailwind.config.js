37b449ba052b30b1935add96fa183ab243716d47
YadavDevCraft <63380531+Y2k-Yadav@users.noreply.github.com>
2026-07-10 16:50:11 +0530

    Add files via upload

---
+++ Tailwind css/PayTM Project/Part-3 Making more cards and More sections Paytm/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "darkblue":"#002970",
+        "profilecolor":"#00BAF2",
+        "orginal":"#00baf2",
+        "lightcolor":"#f5f5f5",
+        "hovercolor":"#00afe3",
+        "hovercolor2":"#0f4a8a",
+        "bgcolor":"#f5f7fa"
+      },
+      width:{
+        "27.5":"6.5rem",
+        "38":"150px",
+        "100":"450px",
+      },
+      height:{
+        "85":"350px",
+      }
+    },
+  },
+  plugins: [],
+}
+
